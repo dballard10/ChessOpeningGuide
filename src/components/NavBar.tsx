6c119480ee61e5aab38086da4a3eaaadac6f1e04
dballard10 <dylanballard55@gmail.com>
2025-02-12 15:25:38 -0500

    Add navbar and logo.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,14 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/chesslogo.webp";
+//import ColorModeSwitch from "./ColorModeSwitch";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <HStack padding="10px">
+      <Image src={logo} width="60px" height="60px" />
+      {/* <ColorModeSwitch /> */}
+    </HStack>
+  );
 };
 
 export default NavBar;
