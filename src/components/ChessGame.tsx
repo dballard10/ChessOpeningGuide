ffe6ee4638424f11c8fe56ae75182b8caffd56be
dballard10 <dylanballard55@gmail.com>
2025-02-16 11:34:14 -0500

    Make logo rounded and add fonts. adjust padding.

--- src/components/ChessGame.tsx
+++ src/components/ChessGame.tsx
@@ -36,7 +36,7 @@ const ChessGame: React.FC<ChessGameProps> = ({
   };
 
   return (
-    <SimpleGrid columns={1} padding={10}>
+    <SimpleGrid columns={1} paddingRight={20}>
       <HStack justifyContent="space-between">
         <Heading>
           <Icon
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,23 @@
-import { Heading, HStack, Image } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/chesslogo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding="10px">
+    <HStack justifyContent={"space-between"} padding="20px">
       <HStack>
-        <Image src={logo} width="60px" height="60px" />
-        <Heading>Chess Opening Guide</Heading>
+        <img
+          src={logo}
+          width="60px"
+          height="60px"
+          style={{ borderRadius: "10px" }}
+        />
+        <h1
+          className="text-xl font-bold"
+          style={{ fontFamily: "Pacifico, cursive" }}
+        >
+          Chess Opening Guide
+        </h1>
       </HStack>
       <ColorModeSwitch />
     </HStack>
