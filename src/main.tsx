5e61c94a54f4e47ead794759b2f7d33863f641a9
dballard10 <dylanballard55@gmail.com>
2025-02-19 13:37:36 -0500

    Remove original css.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme.ts";
-import "./index.css";
 import App from "./App.tsx";
 import { ColorModeProvider } from "./components/ui/color-mode";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider value={theme}>
